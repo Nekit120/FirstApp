89d265084d1e28979ea6587d760c9f9dc9a6863c
Nekit120 <manusa120@mail.ru>
2023-04-04 16:29:18 +0300

    Add entity ShoppingListNames

---
+++ app/src/main/java/com/example/firstapp/entities/ShoppingListNames.kt
@@ -0,0 +1,27 @@
+package com.example.firstapp.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_list_names")
+data class ShoppingListNames(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int?,
+
+    @ColumnInfo(name = "name")
+    val name:String,
+
+    @ColumnInfo(name = "time")
+    val time:String,
+
+    @ColumnInfo(name = "allItemCounter")
+    val allItemCounter:Int,
+
+    @ColumnInfo(name = "checkedItemsCounter")
+    val checkedItemsCounter:Int,
+
+    @ColumnInfo(name = "itemsIds")
+    val itemIds:String
+
+):java.io.Serializable
