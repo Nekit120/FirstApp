057ceda0358f32238645363581740e040d447c94
Nekit120 <manusa120@mail.ru>
2023-04-24 18:41:35 +0300

    Activation result and its creation

--- app/src/main/java/com/example/firstapp/activities/NewNoteActivity.kt
+++ app/src/main/java/com/example/firstapp/activities/NewNoteActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuItem
 import com.example.firstapp.R
 import com.example.firstapp.databinding.ActivityNewNoteBinding
+import com.example.firstapp.fragments.NoteFragment
 
 class NewNoteActivity : AppCompatActivity() {
 
@@ -15,15 +16,34 @@ class NewNoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityNewNoteBinding.inflate(layoutInflater)
         setContentView(bind.root)
+        actionBarSettings()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.nw_note_menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
-
+//обработчик нажатий
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
+     if(item.itemId == R.id.id_save){
+         setMainResult()
+     } else if (item.itemId== android.R.id.home){
+         finish()
+     }
         return super.onOptionsItemSelected(item)
+    }
+// получение результата
+    private fun setMainResult(){
+        val i = intent.apply {
+            putExtra(NoteFragment.TITLE_KEY,bind.edTitle.text.toString())
+            putExtra(NoteFragment.DESCRIPTION_KEY,bind.edDescription.text.toString())
+        }
+    setResult(RESULT_OK,i)
+    finish()
+    }
+
+    private fun actionBarSettings(){
+        val ab = supportActionBar
+        ab?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- app/src/main/java/com/example/firstapp/fragments/NoteFragment.kt
+++ app/src/main/java/com/example/firstapp/fragments/NoteFragment.kt
@@ -2,6 +2,7 @@ package com.example.firstapp.fragments
 
 import android.app.Activity
 import android.content.Intent
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -26,23 +27,36 @@ class NoteFragment : BaseFragment() {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
     override fun onClickNew() {
-        editLauncher.launch(Intent(activity,NewNoteActivity::class.java))
+     editLauncher.launch(Intent(activity,NewNoteActivity::class.java))
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        onEditResult()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
        bind = FragmentNoteBinding.inflate(inflater,container,false)
+
         return bind.root
     }
+
+    //получение результата с активити
     private fun onEditResult(){
-        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        editLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
             if(it.resultCode == Activity.RESULT_OK)
-            {}
+            {
+                bind.TeVe.text=it.data?.getStringExtra(TITLE_KEY)
+            }
         }
     }
     companion object {
+        const val TITLE_KEY = "title_key"
+        const val DESCRIPTION_KEY = "desk_key"
         @JvmStatic
         fun newInstance() = NoteFragment()
 
