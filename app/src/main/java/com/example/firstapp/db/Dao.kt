f41f215fe3296dbd0d42f06a5573958c5018adef
Nekit120 <manusa120@mail.ru>
2023-05-08 17:27:05 +0300

    Database entry ShopListNames

--- app/src/main/java/com/example/firstapp/db/Dao.kt
+++ app/src/main/java/com/example/firstapp/db/Dao.kt
@@ -6,6 +6,7 @@ import androidx.room.Query
 import androidx.room.Update
 
 import com.example.firstapp.entities.NoteItem
+import com.example.firstapp.entities.ShoppingListNames
 import kotlinx.coroutines.flow.Flow
 
 @androidx.room.Dao
@@ -14,12 +15,18 @@ interface Dao {
 
   @Query ("SELECT * FROM note_list")
   fun getAllItems(): kotlinx.coroutines.flow.Flow<List<NoteItem>>
+  @Query ("SELECT * FROM shopping_list_names")
+  fun getAllShopListNames(): kotlinx.coroutines.flow.Flow<List<ShoppingListNames>>
   @Query ("DELETE FROM note_list WHERE id IS :id")
   suspend fun deleteNote(id:Int)
   @Insert
   suspend fun insertNote(note:NoteItem)
   @Update
   suspend fun updateNote(note:NoteItem)
+  @Insert
+  suspend fun insertShopListName(name:ShoppingListNames)
+
+
 
 
 }
